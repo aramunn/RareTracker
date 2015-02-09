48044fee02cb90f6e95639f1e04542b4d091ff4d
Arne De Herdt <arne.de.herdt@gmail.com>
2015-02-09 19:19:36 +0100

    Update RareTracker.lua
    update to v1.3 to include base french and german

--- RareTracker.lua
+++ RareTracker.lua
@@ -53,7 +53,7 @@ function RareTracker:new(o)
   	self.__index = self
 
   	o.majorVersion = 1
-  	o.minorVersion = 1
+  	o.minorVersion = 3
   	o.newRares = false
   	o.rareMobs = {}
   	local strCancelLocale = Apollo.GetString(1);
@@ -79,9 +79,31 @@ function RareTracker:new(o)
 		"Bogus Fraz","Gnashing Cankertube Garr","The Stump","Fool's Gold","NG Protector One","Marmota","Ruga the Ageless","Lightback","Grace","Pusbelly","Randok","Tessa","Flood",
 		"Flametongue","Slab","Final Flight","The Enlightened","Growth","Proliferator","Tainted Drone","Tainted Direweb","Deathbite","The Outsider"}
 	elseif strCancelLocale == "Annuler" then
-    	o.defaultRareNames = {}
+    	o.defaultRareNames = {"Nomjin","Éclat de givre","Petit doigt","Dompteur Xix","Iiksy","Ombrechute","Tas de pierres","Endigueur","Souffletripe","Rongeur","Mortebranche",
+		"Ossanu","Verbois le Courrouceur","Acolytes verbois","Ashwin le Crêtetempête","Claymore XT-9","AG5 Blitzbuster","Nym, vierge de la pitié","Asteria","Acacia","Atethys",
+		"Mikolai le malfaisant","La reine des ombres","Goliath XL-51","Reine Bizzbizze","Capitaine Fripeti","Garde du Tellurixe","Bombardeur RG3","Brigadier Bellza","Assiégeant noir",
+		"Exterminateur Cryvex","Veshra l'œil du cyclone","Renverseur","Gravek le Frappefosse","Veldrok le Vengeur","Moreg le Déchiqueteur","Zersa la Promise","Kalifa","Cromlech l'Enfourné",
+		"Meldrid la Décatie","Cloquepoil","Brise-bourrasque","Retour de flamme","Bave de Grondeur","Docteur Vilserf","Kryne le Brisemarée","Quin fine gâchette","Andara le Devin","Crog le Fracasseur",
+		"Explorateur ER-7","Défenseur AX-12","Torgal le Dévasteur","Corrugriffe","Gorax le Putride","Vieux tas de ferraille","Épouvantos","Gardien Xeltos","Gardien Zelkix","Silex","Gorignak","Poings de granit",
+		"Dreich","Bourdard","Croblanc","Détritus","Viverelle","L'Antre de la reine","Cligneur","Drifter","Le Lobotomiseur","Écran abyssal","Mortes-poignes","Garde Alpha 1","Garde Alpha 2","Souille-lame",
+		"Vorgrim","Le Vautour","Morterelle","Le Ravageur","Amorphomorphe","Roi Sinistroche","Scrabbles","Sergent Garog","Excargo","Primo Gorganoth","Le Flotteur","Arme 24","Spectraileron","Torrent - Reduced movement speed.",
+		"Tourbillon","Soucheflamme","Effroimorille","Régulateur11","Sonde auxiliaire","Sarod l'Insensé","Aeacus","Cornargent","Voresk Branchivenin","La Terreur de Rochesang","Zakan le Nécrochaman","Courroux du Niwha",
+		"Froc de Felidax","Terminus Rex","Gavwyn le Défenseur verdoyant","Mâchoire d'acier","Arianna Herbefolle","Sentinelle d'Arianna","Assassin d'Arianna","Sujets: Rho","La faim sans fin","Nakaz le Seigneur mort",
+		"Sanguifouet","Rouillebec","Donnemort","Grapace","Grouillis","Roi Lacérace","Reine Kizzek","Protectrice du bosquet Fellia","Acèregriffe","Chef de Noircœur","Rondo","Équipe de Rondo","Alpha XT-9",
+		"Crystalback Bull","Rashanna la Buveuse d'âmes","Le Maître des braises","Putrecroc","Maître des sorts Verwyn","SujetV: tempête","SujetJ: démon","SujetK: brute","SentinelleKE-27","Source d'énergieKE-28",
+		"Sujet: Tau","Monture Broyeur","Bugwit","Croc-de-glace","Morglace","Torvex le Titan de cristal","DestructeurK9","Coquetempête","Pilonneur FR2","Permafrost","Drud le Dément","Skorga le Glacé","Seigneur de guerre Nagvox",
+		"Griffegel","Commotionneur","Renverseur","Défenseur AX-12","Dominateur"}
     elseif strCancelLocale == "Abbrechen" then
-    	o.defaultRareNames = {}
+    	o.defaultRareNames = {"Der Schrecken von Blutstein","Zakan der Nekroschamane","Zorn von Niwha","Felidax","Terminus Rex","Gavwyn der Grüne Verteidiger","Stahlkiefer","Arianna Wildgras",
+		"Ariannas Wachposten","Ariannas Assassinin","Subject: Rho","Der unstillbare Hunger","Nakaz der Totenherr","Teufelskerl Braz","Geistflosse","Strömung","Wirbelwind","Flammensippe","Furchtmorchel",
+		"Regulator11","Hilfssonde","Sarod der Sinnlose","Aeacus","Silberhorn","Vorsek Giftkiemen","Blutschwanz","Faulschnabel","Todespfote","Grudder","Quiggles","König Grausamklaue","Königin Kizzek",
+		"Hainbeschützerin","Scharfkralle","Häuptlich Schwarzherz","Rondo","Rondos Trupp","XT-9 Alpha","Kristallrücken","Rashanna die Seelentrinkerin","Der Glutmeister","Faulzahn","Spruchmeister Verwyn",
+		"Kalifa","Cromlech der Brennofengeborene","Seele der Silva","Meldrid die Altersschwache","Blasenfluch","Böe","Flammenwoge","Flammenbinder Sorvel","Rumpeln","Doktor Faulsklave","Kryne der Wellenbrecher",
+		"Quin Flinkhand","Andara die Seherin","Crog der Schmetterer","ER-7 Kundschafter","AX-12 Verteidiger","Schorfklaue","Gorax der Faulige","Torgal der Verwüster","Alte Rumpelkiste","Schreckensknochen",
+		"Wächter Xeltos","Wächter Zelkix","Augmentierter Zornmeister","Steingedärm","Wildklaue","Grug der Scharfrichter","Faulzahn","Spalter Grogek","Flammenbinder Trovin","Königin Tizzet","Feuerstein",
+		"Grignak","Granitfaust","Dreich","Summuel","Weißzahn","Geröll","Lebensgraser","Blutmähne","Die Königin","Blinzli","Gammler","Der Lobotomisierer","Abgrund","Totenpfoten","Alphawache Eins",
+		"Alphawache Zwei","Transmutationsklinge","Vorgrimm"."Der Vultch","Todgraser","Endlosschlund","SubjektV - Sturm","SubjektJ - Scheusal","SubjektK - Widerling","Subjekt V - Sturm","Subjekt J - Scheusal",
+		"Subjekt K - Widerling","Schleifer","Kleinsinn","Wache KE-27","Auflader KE-28", "Subjekt: Tau"}
     else
 		o.defaultRareNames = {}
 	end
